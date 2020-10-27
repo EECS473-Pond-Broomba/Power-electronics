114f0a23c137df8251c1a269c5768642d54032d3
bansarth <sarthakb42@gmail.com>
2020-10-27 16:35:27 -0400

    added updated motor control code

---
+++ motorControl/motor.h
@@ -0,0 +1,46 @@
+#ifndef INC_MOTOR_H_
+#define INC_MOTOR_H_
+
+#include "stm32f4xx_hal.h"
+
+
+//pwm output timers for each of the pwm for each motor
+TIM_HandleTypeDef * htiml1;
+TIM_HandleTypeDef * htiml2;
+TIM_HandleTypeDef * htimr1;
+TIM_HandleTypeDef * htimr2;
+
+/*
+ * This function sets the timers to the initialized timers from the .ioc
+ *
+ */
+void motorInit(TIM_HandleTypeDef * htim1, TIM_HandleTypeDef * htim2,
+		TIM_HandleTypeDef * htim3, TIM_HandleTypeDef * htim4);
+
+
+
+/*
+* Set the motor to a new speed
+* takes in the pwm values for left and right pwm for each motor
+*/
+void setSpeed(int pwml1, int pwml2, int pwmr1, int pwmr2);
+
+/*
+* Changes the direction the motors are driving in
+* direction: A value that distinguishes which exact direction the motors should go in:
+  left forward(0), left back(1), right forward(4), right back (5), forward(2), backwards(3)
+* degreeTurn: The amount of degrees to turn the robot
+*/
+void changeDirection(int direction, int degreeTurn);
+
+/*
+Causes the motors to move forward or backward based on current GPIO Pin State
+*/
+void oppositeDirection();
+
+/*
+Turns motors off by setting the pwm equal to 0
+*/
+void stopMotors();
+
+#endif /* INC_MOTOR_H_ */
